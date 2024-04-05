c0231cc524a51efac512738f4509164ae6863896
Daniel Lowinger <daniel.lowinger@post.runi.ac.il>
2024-04-05 17:40:48 +0300

    fix 6

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -15,6 +15,53 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
 		Random generator = new Random(seed);
+		int two = 0;
+		int three = 0;
+		int more = 0;
+		double sum = 0;
+		String common = "";
+		int j = 1;
+		int i = 0;
+		while (j <= T) {
+			boolean girl = false;
+			boolean boy = false;
+			i = 0;
+			while (boy == false || girl == false) {
+				double x = generator.nextDouble();
+				if (x >= 0.5) {
+					boy = true;
+				} else {
+					girl = true;
+				}
+				i++;
+			}
+			if (i == 2) {
+				two++;
+			}
+			if (i == 3) {
+				three++;
+			}
+			if (i > 3) {
+				more++;
+			}
+			sum = sum + i;
+			j++;
+		}
+		if (two >= three && two >= more) {
+			common = common + "The most common number of children is 2.";
+		}
+		if (three > two && three >= more) {
+			common = common + "The most common number of children is 3.";
+		}
+		if (more > two && more > three) {
+			common = common + "The most common number of children is 4 or more.";
+		}
+		double avg = sum / T;
+		System.out.println("Average: " + avg + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + two);
+		System.out.println("Number of families with 3 children: " + three);
+		System.out.println("Number of families with 4 or more children: " + more);
+		System.out.println(common);
 
 	}
 }
